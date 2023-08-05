8c7a6254dde098b21c78a987269c66d29f2f2e33
송재혁 <wakij6587@naver.com>
2023-08-05 23:36:28 +0900

    refactor: useShaderEffectImage 훅 분리, 변수명 및 함수 리팩토링

--- src/pages/Main/MainPage.tsx
+++ src/pages/Main/MainPage.tsx
@@ -1,7 +1,26 @@
-import Contents from './components/Contents';
+import useShaderEffectImages from 'hook/useShaderEffectImage';
+import Card1 from 'images/card-1.jpg';
+import Card2 from 'images/card-2.jpg';
+import Card3 from 'images/card-3.jpg';
+import Card4 from 'images/card-4.jpg';
+
+const CardList = [Card1, Card2, Card3, Card4];
 
 const MainPage = () => {
-  return <Contents />;
+  useShaderEffectImages();
+
+  return (
+    <main>
+      <div className="scrollable">
+        {CardList.map(src => (
+          <div className="image-container" key={src}>
+            <h1>Threeejs</h1>
+            <img src={src} alt="" />
+          </div>
+        ))}
+      </div>
+    </main>
+  );
 };
 
 export default MainPage;
